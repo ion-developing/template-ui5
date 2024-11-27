1e58ecef2aa48ddd28df8bef3c8e9b484e85f6f1
John Leon <john@quadrant.pe>
2024-11-27 18:56:20 -0500

    Remove unused route handling and user selection logic from Detail controller

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -5,18 +5,6 @@ sap.ui.define(["./BaseController"], function (BaseController) {
         onInit: function () {
 					var oModel = this.getOwnerComponent().getModel();
 					this.getView().setModel(oModel);
-            this.getRouter()
-                .getRoute("detail")
-                .attachPatternMatched(this._onObjectMatched, this);
         },
-
-        _onObjectMatched: function (oEvent) {
-            const userId = oEvent.getParameter("arguments").userId;
-            const oModel = this.getOwnerComponent().getModel();
-            const users = oModel.getProperty("/users");
-            const selectedUser = users.find(user => user.id.toString() === userId);
-
-            oModel.setProperty("/selectedUser", selectedUser);
-        }
     });
 });
